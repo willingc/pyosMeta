d8e6dbd7b1ed2f2b526ef363aefc6966c83d5dae
Carol Willing <carolcode@willingconsulting.com>
2023-12-16 18:31:01 -0800

    comment out __init__ contents

--- src/pyosmeta/__init__.py
+++ src/pyosmeta/__init__.py
@@ -1,16 +1,16 @@
-from .contributors import PersonModel, ProcessContributors
-from .parse_issues import ProcessIssues, ReviewModel
-
-# Trick suggested by flake8 maintainer to ensure the imports above don't
-# get flagged as being "unused"
-__all__ = (
-    "ProcessIssues",
-    "ReviewModel",
-    "PersonModel",
-    "ProcessContributors",
-)
-
-try:
-    from ._version_generated import __version__
-except ImportError:
-    __version__ = "unreleased"
+# from .contributors import PersonModel, ProcessContributors
+# from .parse_issues import ProcessIssues, ReviewModel
+#
+# # Trick suggested by flake8 maintainer to ensure the imports above don't
+# # get flagged as being "unused"
+# __all__ = (
+#     "ProcessIssues",
+#     "ReviewModel",
+#     "PersonModel",
+#     "ProcessContributors",
+# )
+#
+# try:
+#     from ._version_generated import __version__
+# except ImportError:
+#     __version__ = "unreleased"
